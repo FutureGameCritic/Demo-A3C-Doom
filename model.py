ba757a429f5f5b153d581992a00502649d4de067
keicoon <keicoon15@gmail.com>
2018-12-25 17:49:01 +0900

    update gluon model

--- model.py
+++ model.py
@@ -2,56 +2,91 @@ import mxnet as mx
 import mxnet.ndarray as F
 from mxnet import nd, gluon, autograd
 from mxnet.gluon import nn
-
-net = nn.HybridSequential(prefix='MLP_')
-with net.name_scope():
-    net.add(
-        nn.Flatten(),
-        nn.Dense(128, activation='relu'),
-        nn.Dense(64, activation='relu'),
-        nn.Dense(10, activation=None)  # loss function includes softmax already, see below
-    )
-
-ctx = mx.gpu(0) if mx.context.num_gpus() > 0 else mx.cpu(0)
-net.initialize(mx.init.Xavier(), ctx=ctx)
-
-trainer = gluon.Trainer(
-    params=net.collect_params(),
-    optimizer='sgd',
-    optimizer_params={'learning_rate': 0.04},
-)
-
-class Policy(gluon.Block):
+from mxnet.gluon.loss import Loss
+class ConvBlock(gluon.Block):
     def __init__(self, **kwargs):
         super(Policy, self).__init__(**kwargs)
         with self.name_scope():
-            self.dense = nn.Dense(16, in_units=kwargs.input_size, activation='relu')
-            self.action_pred = nn.Dense(kwargs.action_size, in_units=16)
-            self.value_pred = nn.Dense(kwargs.value_size, in_units=16)
+            self.conv1 = nn.Conv2D(16, 8, 4, activation='relu')
+            self.conv2 = nn.Conv2D(32, 4, 2, activation='relu')
+            self.flatten = nn.Flatten()
+            self.fc = nn.Dense(256, activation='relu')
 
     def forward(self, x):
-        x = self.dense(x)
-        probs = self.action_pred(x)
-        values = self.value_pred(x)
-        return F.softmax(probs), values
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.flatten(x)
+        return self.fc(x)
 
-class a3c_global(object):
-    def __init__(self, hparams, env):
+class CrossEntropy(Loss):
+    def __init__(self, _weight = None, _batch_axis= 0, **kwards):
+        Loss.__init__(self,weight=_weight, batch_axis = _batch_axis, **kwards)
+
+    def hybrid_forward(self, action, prob, advantage):
+        action_prob = F.sum(action * prob, axis=1)
+        cross_entropy = F.log(action_prob) * advantage
+        loss = -F.sum(cross_entropy)
+
+        return loss
+
+class A2C(object):
+    def __init__(self, hparams, action_size, value_size):
+        self.hparams = hparams
+
+        self.net_actor, self.net_critic = self.build_model()
+        self.trainer_actor, self.loss_actor, self.trainer_critic, self.loss_critic = self.get_optimizer()
+
+    def build_model(self):
+        actor = ConvBlock()
+        actor.add(nn.Dense(self.hparams.action_size, activation='softmax'))
+        actor.initialize(mx.init.Uniform(0.02))
+
+        critic = ConvBlock()
+        critic.add(nn.Dense(self.hparams.value_size, activation='linear'))
+        critic.initialize(mx.init.Uniform(0.02))
+
+        return actor, critic
+
+    def get_optimizer(self):
+        trainer_actor = gluon.Trainer(self.net_actor.collect_params(), 'adam', {'learning_rate': self.hparams.actor_learning_rate})
+        loss_actor = CrossEntropy()
+
+        trainer_critic = gluon.Trainer(self.net_critic.collect_params(), 'adam', {'learning_rate': self.hparams.critic_learning_rate})
+        loss_critic = gluon.loss.L1Loss()
         
-        # env
-        self.net = self.get_model(
-                        input_size=16,
-                        action_size=2,
-                        value_size=1
-                    )
-        self.net.initialize(mx.init.Uniform(0.02))
-        # trainer = gluon.Trainer(net.collect_params(), 'adam', {'learning_rate': 3e-2})
-        # loss = gluon.loss.L1Loss()
+        return trainer_actor, loss_actor, trainer_critic, loss_critic
 
-    def get_model(self, input_size, action_size, value_size):
-        return Policy(input_size=input_size, action_size=action_size, value_size=value_size)
+    def get_state_from_game(state):
+        out_state = state.screen_buffer
+        return out_state
+        
+    def get_action(self, state):
+        policy = self.net_actor(state)
+        return np.random.choice(self.action_size, 1, p=policy)[0]
 
-class a3c_worker(a3c_global):
-    def __init__(self, hparams):
-        a3c_global.__init__(self, hparams)
-        self.model = None
+    def train_step(self, state, action, reward, next_state, done):
+        value = self.net_critic(state)
+        next_value = self.net_critic(next_state)
+        
+        # one-hot encoding
+        act = np.zeros([1, self.action_size])
+        act[0][action] = 1
+
+        if done:
+            advantage = reward - value
+            target = [reward]
+        else:
+            advantage = (reward + self.hparams.discount_factor * next_value) - value
+            target = reawrd + self.hparams.discount_factor * next_value
+        
+        with autograd.record():
+            # update actor
+            La = self.loss_actor(act, self.net_actor(state), advantage)
+            La.backward()
+            # update critic
+            Lc = self.loss_critic(value, target)
+            Lc.backward()
+
+        # step to trainer
+        self.trainer_actor.step(1)
+        self.trainer_critic.step(1)
--- train.py
+++ train.py
@@ -1,12 +1,58 @@
-from utils.params_loader import loader
+import numpy as np
+from termcolor import colored
 
+from utils.params_loader import loader
 hparams = loader()
-hparams.add_argument('gamma', type=float, default=0.99, help='decay number')
-hparams.add_argument('n_episode', type=int, default=100, help='total episode num')
+hparams.add_argument('n_episode', type=int, default=10, help='total episode num')
+hparams.add_argument('discount_factor', type=float, default=0.99, help='discount factor')
+hparams.add_argument('actor_learning_rate', type=float, default=0.001, help='actor learning rate')
+hparams.add_argument('critic_learning_rate', type=float, default=0.005, help='critic learning rate')
+
+hparams.add_parameter('action_size', 3)
+hparams.add_parameter('value_size', 1)
+
 hparams = hparams.parsing()
 
-# print(hparams.gamma)
-# print(hparams.n_episode)
+print("-------------------------------")
+print(colored("hyper parameters", "green"))
+print(colored(hparams.values(), "green"))
+print("-------------------------------")
 
 # from game import doom
-# from model import a3c
+# game, actions = doom()
+
+# from model import A2C
+# agent = A2C(hparams)
+
+# for e in range(hparams.n_episode):
+#     done = False
+#     game.new_episode()
+
+#     print(colored("Episode #" + str(e + 1), "green"))
+    
+#     while not done:
+#         state = game.get_state()
+        
+#         n = state.number
+#         cur_state = agent.get_state_from_game(state)
+
+#         action = agent.get_action(cur_state)
+        
+#         r = game.make_action(actions[action])
+#         next_state = agent.get_state_from_game(game.get_state())
+#         done = game.is_episode_finished()
+
+#         agent.train_step(cur_state, action, reward, next_state, done)
+
+#         print("State #" + str(n))
+#         print("Reward:", r)
+
+#         # sleep_time = 1.0
+#         # if sleep_time > 0:
+#         #     sleep(sleep_time)
+    
+#     print(colored("Episode finished.", "green")
+#     print(colored("Total reward:{}".format(game.get_total_reward()), "yellow"))
+#     print(" ")
+
+# game.close()
